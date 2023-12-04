414f7570d9067c70df81636a3c9d62e23be6895c
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-12-04 06:30:32 -0800

    correction

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,4 +7,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     element_1_b = tuple_b[0] if len2 > 0 else 0
     element_2_b = tuple_b[1] if len2 > 1 else 0
-    return ("{}".format((element_1_a + element_1_b, element_2_a + element_2_b)))
+    return ("{:d}".format((element_1_a + element_1_b, element_2_a + element_2_b)))
