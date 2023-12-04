e4e9900d9f06eb993327c44b2867f925210c4367
Collins Lekakeny <lekakenycollins8@gmail.com>
2023-12-04 06:34:05 -0800

    fixed sheband

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,10 @@
-#!/usr/in/python3
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     len1 = len(tuple_a)
     len2 = len(tuple_b)
-    element_1_a = tuple_a[0] if len1 > 0 else 0
-    element_2_a = tuple_a[1] if len1 > 1 else 0
+    el_1_a = tuple_a[0] if len1 > 0 else 0
+    el_2_a = tuple_a[1] if len1 > 1 else 0
 
-    element_1_b = tuple_b[0] if len2 > 0 else 0
-    element_2_b = tuple_b[1] if len2 > 1 else 0
-    return ("{:d}".format((element_1_a + element_1_b, element_2_a + element_2_b)))
+    el_1_b = tuple_b[0] if len2 > 0 else 0
+    el_2_b = tuple_b[1] if len2 > 1 else 0
+    return ("{d}".format((el_1_a + el_1_b, el_2_a + el_2_b)))
