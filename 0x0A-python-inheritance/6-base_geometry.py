15713cadc8c3f7ebb2bd9952990ca44d44315774
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-01-08 08:40:05 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Base class
+    Attributes: 
+        public instance method
+    """
+    def area(self):
+        """public instance method
+        Raise: Exception
+        """
+        raise Exception("area() is not implemented")
