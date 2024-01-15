9b1fc6808a398a099a26da9a395150af94859231
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-01-15 06:04:59 -0800

    complete tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,7 +3,8 @@
 
 
 import json
-
+import os
+import csv
 
 class Base:
     """This is the Base class
@@ -41,3 +42,75 @@ class Base:
         with open(filename, "w") as f1:
             j_str = cls.to_json_string([i.to_dictionary() for i in list_objs])
             f1.write(j_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation"""
+        if json_string is None or json_string == "":
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(5, 4)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(3)
+        else:
+            raise ValueError("Unsupported class type")
+        dummy_instance.update(**dictionary)
+        return dummy_instance
+    
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        filename = "{}.json".format(cls.__name__)
+        if not os.path.exists(filename):
+            return []
+        with open(filename, "r") as file:
+            file_content = file.read()
+        list_of_dicts = cls.from_json_string(file_content)
+        instances = [cls.create(**d) for d in list_of_dicts]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """serializes instances to CSV file"""
+        filename = "{}.csv".format(cls.__name__)
+
+        with open(filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    writer.writerow([obj.id, obj.width, obj.height, obj.x, obj.y])
+                elif cls.__name__ == "Square":
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
+                else:
+                    raise ValueError("Unsupported class type")
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """deserializes instances from CSV file"""
+        filename = "{}.csv".format(cls.__name__)
+
+        if not os.path.exists(filename):
+            return []
+
+        instances = []
+
+        with open(filename, mode='r') as file:
+            reader = csv.reader(file)
+
+            for row in reader:
+                if cls.__name__ == "Rectangle":
+                    instance = cls(int(row[0]), int(row[1]), int(row[2]), int(row[3]), int(row[4]))
+                elif cls.__name__ == "Square":
+                    instance = cls(int(row[0]), int(row[1]), int(row[2]), int(row[3]))
+                else:
+                    raise ValueError("Unsupported class type")
+
+                instances.append(instance)
+
+        return instances
