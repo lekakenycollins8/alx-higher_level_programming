22b35b874bd92fe4111d6f944f7ca5f57d64b443
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-02-15 08:50:27 -0800

    10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- lists all shows contained in hbtn_0d_tvshows
+USE hbtn_0d_tvshows;
+SELECT ts.title, tsg.genre_id
+FROM tv_shows AS ts
+INNER JOIN tv_show_genres AS tsg ON ts.id = tsg.show_id
+ORDER BY ts.title AND tsg.genre_id;
