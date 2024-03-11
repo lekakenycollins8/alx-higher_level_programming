4ba98989e343b56584a9f57912225851604fb29b
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-03-11 08:15:55 -0700

    addition

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function add(a, b) {
+	a = parseInt(a);
+	b = parseInt(b);
+
+	return (a + b);
+}
+
+const first_int = process.argv[2];
+const second_int = process.argv[3];
+
+if (first_int && second_int) {
+	console.log(add(first_int, second_int));
+}
+else {
+	console.log(NaN);
+}
