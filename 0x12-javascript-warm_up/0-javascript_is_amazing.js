97e3f7e8a4648bc33a330624800f955d4887db2f
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-03-11 04:56:42 -0700

    corrections

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
-/**
- * script that prints “JavaScript is amazing”
- * You are not allowed to use var
- */
-
 let myVar = "JavaScript is amazing";
+
 console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
-/**
- * Prints 3 lines
- */
-
 console.log("C is fun");
 console.log("Python is Cool");
 console.log("Javascript is amazing");
