d6889dcd73cc6df3fff20f689ff96ffeaa53a9e0
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-03-11 08:26:07 -0700

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function factorial(N) {
+	if (N === NaN || N === 1)
+	{
+		return 1;
+	}
+	return N * factorial(N - 1);
+}
+
+const first_arg = process.argv[2];
+
+if (first_arg) {
+	console.log(factorial(first_arg));
+}
+else {
+	console.log(1);
+}
