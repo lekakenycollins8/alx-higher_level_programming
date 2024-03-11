c63defe4f0d3652f1ccfaa98efe4159cdd07c0fa
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-03-11 08:38:54 -0700

    print 2nd biggest integer

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const integers = process.argv.slice(2).map(Number);
+
+integers.sort((a, b) => b - a)
+
+if (integers.length === 0 || integers.length === 1) {
+	console.log(0);
+}
+else {
+	console.log(integers[1]);
+}
