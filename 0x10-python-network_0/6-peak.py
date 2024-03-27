b9440eeaa72d251832c73f2a682417c5b04d6f28
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-03-27 04:51:09 -0700

    find the peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module to find the peak in a list of integers"""
+
+def find_peak(list_of_integers):
+    """finds peak in list of integers"""
+    if not list_of_integers:
+        return None
+    left = 0 
+    right = len(list_of_integers) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+    return list_of_integers[left]
