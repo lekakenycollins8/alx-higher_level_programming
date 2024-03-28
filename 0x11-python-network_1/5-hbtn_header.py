3775d18807ac6c768bfcc288a4d904f352de55f4
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-03-28 04:49:09 -0700

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""sends a request to URL and displays the value of X-Request-Id"""
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = requests.get(url)
+    header_value = req.headers['X-Request-Id']
+    print(header_value)
