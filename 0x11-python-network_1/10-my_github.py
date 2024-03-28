a448e9af07d3dac915dafd70b24762b949923ba2
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-03-28 06:39:26 -0700

    100-github_commits.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""takes your GitHub(username and password) and uses the GitHub API to display id"""
+"""takes your GitHub(username and pat) and uses the GitHub API to display id"""
 
 
 from sys import argv
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
     username = argv[1]
     token = argv[2]
+    auth = HTTPBasicAuth(username, token)
     url = "https://api.github.com/user"
-    headers = {
-            'Authorization': "Bearer {}".format(token),
-            'Accept': 'application/vnd.github+json'
-            }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, auth=auth)
     if response.status_code == 200:
         user_data = response.json()
         user_id = user_data.get('id')
         print(user_id)
     else:
-        print("Failed to retrieve user ID. Status code: {}".format(response.status_code))
+        print("Failed to retrieve ID. code: {}".format(response.status_code))
---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lists 10 commits from repository "rails" by user: rails"""
+
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    repo_name = argv[1]
+    owner_name = argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            owner_name, repo_name)
+    response = requests.get(url)
+    user_commit = response.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(user_commit[i].get('sha'), 
+                user_commit[i].get('commit').get('author').get('name')))
+    except IndexError:
+        pass
