865498cb63adb25d48c2ed6dcf3c15ea4bbc641b
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-03-28 03:09:24 -0700

    decoded utf8 content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,11 @@
 
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    page = response.read()
-print("Body response:")
-print("\t- type:", type(page))
-print("\t- content:", page)
-print("\t- utf8 content:", page)
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        page = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(page)))
+    print("\t- content: {}".format(page))
+    print("\t- utf8 content: {}".format(page.decode('utf8')))
