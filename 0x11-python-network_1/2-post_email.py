e8a4f8883c336e4ab55b913dbb56764aa17943e4
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-03-28 03:46:29 -0700

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script that sends a POST request to passed URL with email as parameter"""
+
+
+from sys import argv
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = argv[1]
+    email_value = {'email': argv[2]}
+    data = urllib.parse.urlencode(email_value)
+    data = data.encode('utf8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        page = response.read().decode('utf8')
+    print("Your email is: {}".format(page))
