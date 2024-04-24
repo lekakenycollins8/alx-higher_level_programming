159f9562399907a3f7a423f5e69d3f1d78a8ca23
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-04-24 02:29:29 -0700

    get request with request module

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,8 +6,7 @@ const filePath = process.argv[2];
 const content = process.argv[3];
 
 fs.writeFile(filePath, content, 'utf8', (err) => {
-	if (err) {
-		console.log(err);
-	}
+  if (err) {
+    console.log(err);
+  }
 });
-
